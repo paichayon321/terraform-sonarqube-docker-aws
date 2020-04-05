5391ce8917d4e2619c39a3a13904b421ac7043b0
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 14:43:32 +0700

    Update and rename resources.tf to main.tf

---
+++ main.tf
@@ -0,0 +1,66 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-poc-3436"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+# Use AWS Terraform provider
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# Create EC2 instance
+resource "aws_instance" "default" {
+  ami                    = var.ami
+  count                  = var.instance_count
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.default.id]
+  source_dest_check      = false
+  user_data = "${file("run-selenium.sh")}"
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "Selenium Grid"
+  }
+}
+
+resource "aws_security_group" "default" {
+  name = "selenium-terraform-sg"
+  ingress {
+    protocol  = "TCP"
+    from_port = 22
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4444
+    to_port     = 4444
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}

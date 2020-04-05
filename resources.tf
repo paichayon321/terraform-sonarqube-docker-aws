8db5c36ff89e9c7f46925c75b69f0d04fb5c902d
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 14:11:06 +0700

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -12,6 +12,7 @@ module "selenium_grid_host" {
   monitoring             = true
   vpc_security_group_ids = ["${aws_default_security_group.default.id}"]
   subnet_id              = "${var.AWS_SUBNET}"
+  user_data = "${file("install_nginx.sh")}"
 
   name                   = "Selenium Grid"
 
@@ -22,27 +23,4 @@ module "selenium_grid_host" {
   }
 }
 
-module "web_app_host" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "1.21.0"
-
-  associate_public_ip_address = true
-
-  instance_count         = 1
-
-  ami                    = "${data.aws_ami.latest-ubuntu.id}"
-  instance_type          = "t2.micro"
-  key_name               = "${var.AWS_PEM_KEY}"
-  monitoring             = true
-  vpc_security_group_ids = ["${aws_default_security_group.default.id}"]
-  subnet_id              = "${var.AWS_SUBNET}"
-
-  name                   = "Web App"
-
-  tags = {
-    Terraform = "true"
-    Environment = "${var.APP_ENV}"
-    Name                   = "Web App"
-  }
-}
 

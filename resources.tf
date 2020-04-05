419ba8cc21192c4486fcd7d4a4f9626478745208
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 14:16:10 +0700

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,26 +1,15 @@
-module "selenium_grid_host" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "1.21.0"
-
-  associate_public_ip_address = true
-
-  instance_count         = 1
-
-  ami                    = "${data.aws_ami.latest-ubuntu.id}"
-  instance_type          = "t2.micro"
-  key_name               = "${var.AWS_PEM_KEY}"
-  monitoring             = true
-  vpc_security_group_ids = ["${aws_default_security_group.default.id}"]
-  subnet_id              = "${var.AWS_SUBNET}"
+# Create EC2 instance
+resource "aws_instance" "default" {
+  ami                    = var.ami
+  count                  = var.instance_count
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.default.id]
+  source_dest_check      = false
   user_data = "${file("run-selenium.sh")}"
-
-  name                   = "Selenium Grid"
+  instance_type          = var.instance_type
 
   tags = {
-    Terraform = "true"
-    Environment = "${var.APP_ENV}"
-    Name                   = "Selenium Grid"
+    Name = "Selenium Grid"
   }
 }
-
-

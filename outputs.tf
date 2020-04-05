9f1fe7de1f0bda6f9140d1af4ccc98618cd9f715
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 14:20:36 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,4 @@
-output "Selenium Grid Public DNS" {
-  value = "${module.selenium_grid_host.public_dns}"
+output "instance_ips" {
+  value = ["${aws_instance.default.*.public_ip}"]
 }
 
-output "Web App Public DNS" {
-  value = "${module.web_app_host.public_dns}"
-}
-
-output "SSH pem key" {
-  value = "${var.AWS_PEM_KEY}"
-}

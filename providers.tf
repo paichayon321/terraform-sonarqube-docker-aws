caeedeb5e1690b4a2c1c8a4f5f6cf8d0bf6c1947
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-05 14:07:41 +0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-poc-3436"
+    key    = "terraform.selenium-tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
 # Configure the AWS Provider
 provider "aws" {
   region     = "${var.AWS_REGION}"

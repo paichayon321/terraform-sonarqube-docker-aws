ff645d995541cb4388321a0cc9ec85579876c6ce
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2020-04-06 23:54:03 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "instance_ips" {
   value = ["${aws_instance.default.*.public_ip}"]
 }
 
+output "address" {
+  value = "${aws_instance.default.private_ip}"
+}
+
+output "elastic ip" {
+  value = "${aws_eip.default.public_ip}"
+}
